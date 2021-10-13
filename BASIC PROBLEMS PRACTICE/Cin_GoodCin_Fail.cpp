f3736a95829c1dead59227f1f28ab17a2edf0cdb
shubham488-creator <73779227+shubham488-creator@users.noreply.github.com>
2021-10-13 18:13:08 +0530

    13 Oct 2021

---
+++ BASIC PROBLEMS PRACTICE/Cin_GoodCin_Fail.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  double result = 0;
+  double input;
+  while (true)
+    {
+      cout << "Enter a number to add to sum. ";
+      cout << "Or enter a non-number to quit and calculate sum." << endl;
+      cin >> input;
+      if (cin.good ())
+	{
+	  result += input;
+	}
+      if (cin.fail ())
+	{
+	  cout << "Sum = " << result << endl;
+	  break;
+	}
+    }
+
+  return 0;
+}
