5208df1ad09fcea3198d799969b7785ec05911cf
SHUBHAM TIWARI <73779227+shubham488-creator@users.noreply.github.com>
2022-04-04 16:46:49 +0530

    Create Pair1.cpp

---
+++ C++ STL/STL PAIR/Pair1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<utility>
+using namespace std;
+
+int main()
+{
+  pair<int,char> pair1;
+  
+  pair1.first=100;
+  pair1.second='G';
+  
+  cout<<pair1.first<<" "<<pair1.second<<endl;
+  
+  return 0;
+} 
