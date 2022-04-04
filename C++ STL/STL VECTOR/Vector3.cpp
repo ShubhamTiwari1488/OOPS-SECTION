4e64107573183d92f6fe21bd1fed8e177d01adfa
SHUBHAM TIWARI <73779227+shubham488-creator@users.noreply.github.com>
2022-04-04 17:11:12 +0530

    Create Vector3.cpp

---
+++ C++ STL/STL VECTOR/Vector3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+  vector<int> g1;
+  
+  for(int i=1;i<=10;i++)
+    g1.push_back(i*10);
+  
+  cout<<g1[2]<<endl;
+  
+  cout<<g1.at(2)<<endl;
+  
+  cout<<g1.front()<<endl;
+  
+  cout<<g1.back()<<endl;
+  
+  int *pos = g1.data; //Pointer to first element
+  
+  cout<<*pos<<endl;
+  
+  return 0;
+}
