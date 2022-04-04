48b41ff8cad1610b0596398679c03662f8600328
SHUBHAM TIWARI <73779227+shubham488-creator@users.noreply.github.com>
2022-04-04 15:19:42 +0530

    Create List6.cpp

---
+++ C++ STL/STL LINKEDLIST/List6.cpp
@@ -0,0 +1,22 @@
+//list1.merge(list2) function in C++ STL list
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	// declaring the lists
+	// initially sorted
+	list<int> list1 = { 10, 20, 30 };
+	list<int> list2 = { 40, 50, 60 };
+
+	// merge operation
+	list2.merge(list1);
+
+	cout << "List: ";
+
+	for (auto it = list2.begin(); it != list2.end(); ++it)
+		cout << *it << " ";
+
+	return 0;
+}
